c6a42b032fc8b9a29cc9a2fa609041c76934d2ec
shahriar021 <shahriarc19@gmail.com>
2024-12-07 11:31:27 +0600

    a

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <div className="App">
         <Header />
+        <About/>
         <Routes>
           <Route path="/about" element={<About />} />
           
